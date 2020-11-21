38262ef17a73843f3f05e2f691d36183ac725a68
se-jun <dhpassion@naver.com>
2020-11-21 11:17:34 +0900

    pollution 작업중

---
+++ src/main/java/com/ssafy/happyhouse/controller/PollutionController.java
@@ -0,0 +1,11 @@
+package com.ssafy.happyhouse.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/pollution")
+public class PollutionController {
+
+	
+}
