638a60be46036bc0bb88c43b84cdcf4cbb4cda0f
se-jun <dhpassion@naver.com>
2020-11-21 16:44:43 +0900

    Pollution Service create

--- src/main/java/com/ssafy/happyhouse/controller/PollutionController.java
+++ src/main/java/com/ssafy/happyhouse/controller/PollutionController.java
@@ -1,11 +1,35 @@
 package com.ssafy.happyhouse.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.ssafy.happyhouse.model.SidoGugunCodeDto;
+import com.ssafy.happyhouse.model.service.PollutionService;
 
 @Controller
 @RequestMapping("/pollution")
 public class PollutionController {
 
+	@Autowired
+	private PollutionService pollutionService;
+	
+	@GetMapping(value = "", headers = { "Content-type=application/json" })
+	public List<SidoGugunCodeDto> getSido() throws Exception {
+		System.out.println(pollutionService.getSido());
+		return pollutionService.getSido();
+	}
+
+	@ResponseBody
+	@GetMapping(value = "/{sido}", headers = { "Content-type=application/json" })
+	public List<SidoGugunCodeDto> getGugun(@PathVariable String sido) throws Exception {
+		System.out.println(pollutionService.getGugunInSido(sido));
+		return pollutionService.getGugunInSido(sido);
+	}
 	
 }
--- src/main/java/com/ssafy/happyhouse/model/dao/PollutionDao.java
+++ src/main/java/com/ssafy/happyhouse/model/dao/PollutionDao.java
@@ -3,9 +3,12 @@ package com.ssafy.happyhouse.model.dao;
 import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 import com.ssafy.happyhouse.model.PollutionDto;
+import com.ssafy.happyhouse.model.SidoGugunCodeDto;
 
 @Mapper
 public interface PollutionDao {
 	
-	List<PollutionDto> getDong() throws Exception;
+	List<SidoGugunCodeDto> getSido() throws Exception;
+	List<SidoGugunCodeDto> getGugunInSido(String sido) throws Exception;
+	List<PollutionDto> getPollutionInDong(String gugun) throws Exception;
 }
