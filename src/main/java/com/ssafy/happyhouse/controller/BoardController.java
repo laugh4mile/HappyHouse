602bfe40842f478e9e059ae79ff503a8d58da697
yhs <laugh4mile>
2020-11-22 16:33:52 +0900

    Q&A 게시판 Dto, controller, service, serviceImpl, Dao, mapper 생성 sidebar 의 동네정보 밑에 Q&A 링크 생성하여 연결할 예정 pwdfind 의 깨짐 현상 수정

---
+++ src/main/java/com/ssafy/happyhouse/controller/BoardController.java
@@ -0,0 +1,76 @@
+package com.ssafy.happyhouse.controller;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ssafy.happyhouse.model.BoardDto;
+import com.ssafy.happyhouse.model.service.BoardService;
+
+
+
+@RestController
+@RequestMapping("/board")
+public class BoardController {
+
+	private static final Logger logger = LoggerFactory.getLogger(BoardController.class);
+	private static final String SUCCESS = "success";
+	private static final String FAIL = "fail";
+
+	@Autowired
+	private BoardService boardService;
+
+	@GetMapping
+	public ResponseEntity<List<BoardDto>> retrieveBoard() throws Exception {
+		logger.debug("retrieveBoard - 호출");
+		return new ResponseEntity<List<BoardDto>>(boardService.retrieveBoard(), HttpStatus.OK);
+	}
+
+	@GetMapping("{no}")
+	public ResponseEntity<BoardDto> detailBoard(@PathVariable int no) {
+		logger.debug("detailBoard - 호출");
+		return new ResponseEntity<BoardDto>(boardService.detailBoard(no), HttpStatus.OK);
+	}
+
+	@PostMapping
+	public ResponseEntity<String> writeBoard(@RequestBody BoardDto board) {
+		logger.debug("writeBoard - 호출"); 
+		if (boardService.writeBoard(board)) { 
+			return new ResponseEntity<String>(SUCCESS, HttpStatus.OK);
+		}
+		return new ResponseEntity<String>(FAIL, HttpStatus.NO_CONTENT);
+	}
+
+	@PutMapping("{no}")
+	public ResponseEntity<String> updateBoard(@PathVariable int no, @RequestBody BoardDto board) {
+		logger.debug("updateBoard - 호출");
+		logger.debug("" + board);
+		board.setNo(no);
+		
+		if (boardService.updateBoard(board)) {
+			return new ResponseEntity<String>(SUCCESS, HttpStatus.OK);
+		}
+		return new ResponseEntity<String>(FAIL, HttpStatus.NO_CONTENT);
+	}
+
+	@DeleteMapping("{no}")
+	public ResponseEntity<String> deleteBoard(@PathVariable int no) {
+		logger.debug("deleteBoard - 호출");
+		if (boardService.deleteBoard(no)) {
+			return new ResponseEntity<String>(SUCCESS, HttpStatus.OK);
+		}
+		return new ResponseEntity<String>(FAIL, HttpStatus.NO_CONTENT);
+	}
+}
