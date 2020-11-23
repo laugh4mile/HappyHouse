47613d19cb14e789f6deb03b2f37f3c389803548
JUNG <JUNG@JUNG-IDEAPAD330S>
2020-11-23 17:13:03 +0900

    edit mypage -> interestsList 페이지 이동 확인

--- src/main/java/com/ssafy/happyhouse/controller/InterestsController.java
+++ src/main/java/com/ssafy/happyhouse/controller/InterestsController.java
@@ -1,5 +1,22 @@
 package com.ssafy.happyhouse.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+import com.ssafy.happyhouse.model.service.InterestsService;
+
+@Controller
+@RequestMapping("/interests")
 public class InterestsController {
+//	@Autowired
+//	private InterestsService interestsService;
+
+	@GetMapping(value = "/list")
+	public String test3() {
+		return "dist/InterestsList";
+	}
 
 }
