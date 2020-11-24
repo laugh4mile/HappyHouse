b79e097913522c536d9cf658d602f36546acb22e
JUNG <JUNG@JUNG-IDEAPAD330S>
2020-11-24 09:07:28 +0900

    edit interestsList.jsp에 관심목록 띄우기

--- src/main/java/com/ssafy/happyhouse/model/service/InterestsService.java
+++ src/main/java/com/ssafy/happyhouse/model/service/InterestsService.java
@@ -1,5 +1,10 @@
 package com.ssafy.happyhouse.model.service;
 
-public class InterestsService {
+import java.util.List;
 
+import com.ssafy.happyhouse.model.InterestsDto;
+
+public interface InterestsService {
+	List<InterestsDto> getInterestsList(String email);
+	void delete(String email, int no);
 }
